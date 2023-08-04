f6ba512b846d1c34e6dac810f91597f4490702a3
Professor-Rafael <101332916+professor-rafael@users.noreply.github.com>
2023-08-04 20:07:02 -0300

    adicionando csse js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+
+    createCanvas(200, 200);
+    
+    background("blue");
+    
+    }
+    
+    function draw() {
+    
+    stroke("orange");
+    
+    triangle(50, 150, 100, 50, 150, 150);
+    
+    }
+    
